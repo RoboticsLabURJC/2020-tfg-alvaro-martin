ae073e5d063f99b3a1e23b1c73bdfcf587a76f03
Martinnello <ammenacho@hotmail.com>
2021-02-15 00:54:57 +0100

    folder error solved
    need more raw videos, not working with all

--- Extract Frames/Frames_extract.py
+++ Extract Frames/Frames_extract.py
@@ -4,54 +4,47 @@ import os
 import numpy as np
 
 folder_path = '/Users/Martin/Desktop/TFG/Proyecto Github/2020-tfg-alvaro-martin/Extract Frames/'
-video_name = 'MVI_2481.MP4'
+video_name = 'MVI_2517.MP4'
 video_path = folder_path + video_name
 
 frames_path = folder_path + '/frames' + '_' + video_name
-is_path = os.path.isdir(frames_path)
-print(is_path)
-if is_path == False:
-    os.mkdir(frames_path)
-    cap = cv2.VideoCapture(video_path)
-    os.chdir(frames_path)
-    img_index = 0
+os.makedirs(frames_path,exist_ok=True)
+cap = cv2.VideoCapture(video_path)
+os.chdir(frames_path)
+img_index = 0
 
-    while (cap.isOpened()):
-        ret, frame = cap.read()
-        if ret == False:
-            break
-        gray_image = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        ret,binary = cv2.threshold(gray_image,127,255,0)
-        width = 120
-        height = 80
-        dim = (width, height)
+while (cap.isOpened()):
+    ret, frame = cap.read()
+    if ret == False:
+        break
+    gray_image = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    ret,binary = cv2.threshold(gray_image,127,255,0)
+    width = 120
+    height = 80
+    dim = (width, height)
 
-        # resize image
-        resized = cv2.resize(binary, dim, interpolation = cv2.INTER_AREA)
+    # resize image
+    resized = cv2.resize(binary, dim, interpolation = cv2.INTER_AREA)
 
-        # calculate moments of binary image
-        M = cv2.moments(resized)
-        # calculate x,y coordinate of center
+    # calculate moments of binary image
+    M = cv2.moments(resized)
+    # calculate x,y coordinate of center
 
-        cX = int(M["m10"] / M["m00"])
-        cY = int(M["m01"] / M["m00"])
+    cX = int(M["m10"] / M["m00"])
+    cY = int(M["m01"] / M["m00"])
 
-        # put text and highlight the center
-        black_image = np.zeros((80,120,3), np.uint8)
+    # put text and highlight the center
+    black_image = np.zeros((80,120,3), np.uint8)
 
-        #cv2.circle(black_image, (cX, cY), 1, (255, 255, 255), 1)
-        print (cX,cY)
-        if (cY <= 120) and (cX <= 80):
-            black_image[(cY, cX)] = 255
-            if img_index >= 135 and img_index <= 180:
-            cv2.imwrite('video_frames' + str(img_index) + '.png', black_image )
+    #cv2.circle(black_image, (cX, cY), 1, (255, 255, 255), 1)
+    print (img_index,cX,cY)
+    if (cY <= 120) and (cX <= 80):
+        black_image[(cY, cX)] = 255
+        #if img_index >= 135 and img_index <= 180:
+        cv2.imwrite('video_frames' + str(img_index) + '.png', black_image )
 
-        img_index += 1
+    img_index += 1
+    #if ((img_index % 10) == 0):
 
-        #if ((img_index % 10) == 0):
-
-    cap.release()
-    cv2.destroyAllWindows()
-
-else:
-    print('The folder already exist')
+cap.release()
+cv2.destroyAllWindows()
