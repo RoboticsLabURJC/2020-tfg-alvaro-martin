bf7c2f5b92e252ba95bd36303fc72cb504f6c647
Martinnello <ammenacho@hotmail.com>
2021-10-18 18:00:36 +0200

    ok now

--- Live_camera.py
+++ Live_camera.py
@@ -7,7 +7,7 @@ while (True):
   ret, frame = cap.read()
   cv2.imshow('Visor', frame)
 
-  if cv2.watiKey(1) & 0xFF == ord('q'):
+  if cv2.waitKey(1) & 0xFF == ord('q'):
     break
 
 cap.realease()
